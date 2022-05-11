7f0d181ddc0b6e22344f449c6413fe181370255a
Suryavijay Dhanasekaran <suryavjsv@gmail.com>
2022-05-11 14:04:53 +0530

    Initial Commit with example code

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# Matterport_demo_elxsi
+# Matterport SDK 2021 Tutorial
+_Written by LE TEXIER Loic for [Meister Guild](https://m-gild.com)_
+
+## Install
+Install the Node.js modules:
+
+```
+npm install
+```
+## Server
+To launch the Express server, run:
+
+```
+node server.js
+```
+## Samples
+Try the samples:
+
+- [Matterport SDK for embeds](https://localhost:8000) _(A model with Mattertags)_
+- [Matterport SDK Bundle](https://localhost:8000/bundle) _(A model with an animated object inside)_
+- [Intersection Inspector](https://localhost:8000/inspector)
+- [Simple Editor](https://localhost:8000/editor) _(A simplified version of the Transient Tag Editor)_
+- [Transient Tag Editor](https://localhost:8000/editor-full)
+
+## Tutorial
+This is the backup of the Tutorial I wrote on Qiita:
+
+- [English](tutorial.md)
+- [日本語](チュートリアル.md)
---
+++ server.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')))
+app.get('/', (req, res) => {
+    res.sendFile(`${__dirname}/public/index.html`);
+});
+app.get('/bundle', (req, res) => {
+    res.sendFile(`${__dirname}/public/index-bundle.html`);
+});
+app.get('/editor', (req, res) => {
+    res.sendFile(`${__dirname}/public/index-editor.html`);
+});
+app.get('/editor-full', (req, res) => {
+    res.sendFile(`${__dirname}/public/index-editor-full.html`);
+});
+app.get('/inspector', (req, res) => {
+    res.sendFile(`${__dirname}/public/index-inspector.html`);
+});
+app.get('/picture', (req, res) => {
+    res.sendFile(`${__dirname}/public/index-picture.html`);
+});
+app.listen(8000, () => {
+    console.log('Application running on port 8000!');
+});
